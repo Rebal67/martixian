e992cf5cc7bc6722625e6b84ca927086a324e24b
rebal67 <rebalree1@gmail.com>
2023-07-03 11:36:12 +0200

    add comments

--- frontend/js/ui-service.js
+++ frontend/js/ui-service.js
@@ -7,13 +7,18 @@ class UiService {
   constructor(categoryService) {
     this.#categoryService = categoryService;
   }
-
+  /**
+   * initiate the app and fetches the data
+   */
   async init() {
     this.#categoryService.fetch();
     this.#categories = this.#categoryService.categories;
     this.#bootstrap();
   }
 
+  /**
+   * render or rerender the Selects
+   */
   #bootstrap() {
     this.#selects = this.createSelects();
     const container = document.querySelector("#container");
@@ -21,6 +26,10 @@ class UiService {
     container.append(...this.#selects);
   }
 
+  /**
+   * create array of select element and putting default selected value
+   * @returns array of select element
+   */
   createSelects() {
     const selects = [];
     for (const [level, categories] of this.#categories.entries()) {
@@ -31,6 +40,12 @@ class UiService {
     return selects;
   }
 
+  /**
+   * create select element and add even listeners on change
+   * @param {number} level level of the selects
+   * @param {category} categories the categories to be used in the options of the select
+   * @returns select element
+   */
   #createSelect(level, categories) {
     const select = document.createElement("select");
     select.dataset.level = level;
@@ -46,18 +61,22 @@ class UiService {
     return select;
   }
 
+  /**
+   * handles event when select element changes
+   * @param {changeEvent} event select change event
+   */
   #selectListener(event) {
     const level = parseInt(event.target.dataset.level);
     const id = parseInt(event.target.value);
 
     const node = this.#categoryService.findNode(id);
     const parents = this.#categoryService.getAllParents(node).reverse();
-    this.#selected = [...parents.map((parent) => parent.id), id];
     const children = this.#categoryService.getChildrenMappedByLevel(
       node.children,
       level
     );
 
+    this.#selected = [...parents.map((parent) => parent.id), id];
     const filteredCategories = [];
     const currentFilteredNode =
       parents[parents.length - 1]?.children ??
@@ -72,10 +91,21 @@ class UiService {
     this.#selects[level].value = id;
   }
 
+  /**
+   * sets selected value of select element if exists
+   * @param {HTMLSelectElement} select select element
+   * @param {number} index index of the select
+   */
   $setSelected(select, index) {
     if (this.#selected[index]) select.value = this.#selected[index];
   }
 
+  /**
+   * creates option element
+   * @param {number} value the value of the option element
+   * @param {string} name  name of the option element
+   * @returns HTMLOptionElement - created option element
+   */
   #createOption(value, name) {
     const option = document.createElement("option");
     option.value = value;
@@ -83,6 +113,9 @@ class UiService {
     return option;
   }
 
+  /**
+   * resets selects to default behavior
+   */
   reset() {
     this.#categories = this.#categoryService.categories;
     this.#selected = [];
